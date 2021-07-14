85d1579401d300c5ad4eaea8740ee774b6569736
GMDEEP <stewartjgreen@gmail.com>
2021-07-14 00:30:57 +0000

    First Update

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var topCard = document.querySelector("#topCard");
+  var middleCard = document.querySelector("#middleCard");
+  var bottomCard = document.querySelector("#bottomCard");
+  var displaySuite = topCard,
+    bottomCard;
+  var suitesArray = ["♦", "♥", "♠", "♣"];
+  var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  var randomSuite = Math.floor(Math.random() * suitesArray.length);
+  var randomNum = Math.floor(Math.random() * numArray.length);
+  topCard.innerHTML = "♥";
+  //   console.log("Hello Rigo from the console!");
 };
